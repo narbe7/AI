e2060696789811ec45c9d5532718b92e2871cb03
Narbeh <narbe.ha@gmail.com>
2025-01-20 22:28:16 +0330

    First gui version of AI

--- AI.py
+++ AI.py
@@ -1,7 +1,32 @@
 from customtkinter import *
-# window = CTk()
+
 from sklearn import tree
 import csv
+
+window = CTk()
+window.geometry("500x500+450+200")
+window.resizable(False, False)
+window.title("Narbeh's AI app")
+lbl = CTkLabel(window, text="")
+lbl.pack()
+lable = CTkLabel(window, text="Hello this is my first AI app", font=('Helvetica', 22, 'bold'))
+lable.pack()
+labletemp = CTkLabel(window, text="")
+labletemp.pack()
+lable1 = CTkLabel(window, text="Please enter your Height: ")
+lable1.pack()
+input_height = CTkEntry(window)
+input_height.pack()
+lable2 = CTkLabel(window, text="Please enter your Weight: ")
+lable2.pack()
+input_weight = CTkEntry(window)
+input_weight.pack()
+lable3 = CTkLabel(window, text="Please enter your shoes size: ")
+lable3.pack()
+input_shoes = CTkEntry(window)
+input_shoes.pack()
+lable4 = CTkLabel(window, text="")
+lable4.pack()
 
 x = []
 y = []
@@ -12,10 +37,21 @@ with open('list.csv', 'r') as csvfile:
         y.append(i[3])
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(x, y)
-aa = int(input('Enter your Height: '))
-bb = int(input('Enter your Weight: '))
-cc = int(input('Enter your shoes size: '))
-answer = [aa, bb, cc]
-result = clf.predict([answer])
-print(f'I think you are a {result}')
-# window.mainloop()
+
+
+def res():
+    h = int(input_height.get())
+    w = int(input_weight.get())
+    s = int(input_shoes.get())
+    answer = [h, w, s]
+    result = clf.predict([answer])
+    lable6 = CTkLabel(window, text=f'I think you are a {str(result[0]).upper()}', font=('Helvetica', 20, 'bold'))
+    lable6.pack()
+
+
+button = CTkButton(window, text="see result", command=res)
+button.pack()
+lable5 = CTkLabel(window, text="")
+lable5.pack()
+
+window.mainloop()
